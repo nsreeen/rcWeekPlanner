73a53c368486e9d70256f1ad50abcfe5fe1d3912
Nasreen </>
2022-08-23 15:42:08 +0100

    Remove some unnecessary code

--- src/main/kotlin/com/example/models/Time.kt
+++ src/main/kotlin/com/example/models/Time.kt
@@ -1,6 +1,6 @@
 package com.example.models
 
-import java.time.DayOfWeek
+import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.ZoneOffset
 import java.time.ZonedDateTime
@@ -10,17 +10,9 @@ import java.time.temporal.ChronoUnit
 data class Time(val utcDateTime: String, val dayOfWeek: String)
 
 fun utcIsoStringFromRcIcsString(s: String): Time {
-    val estTime: ZonedDateTime = getZonedTime(
-        year=s.substring(0,4).toInt(),
-        month=s.substring(4,6).toInt(),
-        dayOfMonth=s.substring(6,8).toInt(),
-        hour=s.substring(9,11).toInt(),
-        minute=s.substring(11,13).toInt(),
-        second=0,
-        nanoOfSecond=0,
-        zone=ZoneId.of("America/New_York")
-    )
-    //val utcTime: ZonedDateTime = estTime.withZoneSameInstant(ZoneId.of("Etc/UTC"))
+    val formatter = DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmss")
+    val ldt: LocalDateTime = LocalDateTime.parse(s, formatter)
+    val estTime: ZonedDateTime = ZonedDateTime.of(ldt, ZoneId.of("America/New_York"))
     val utcTime: ZonedDateTime = estTime.withZoneSameInstant(ZoneOffset.UTC)
     return Time(
         utcDateTime=utcTime.toString(),//DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(utcTime),
@@ -28,21 +20,6 @@ fun utcIsoStringFromRcIcsString(s: String): Time {
     )
 }
 
-fun getZonedTime(
-    year: Int,
-    month: Int,
-    dayOfMonth: Int,
-    hour: Int,
-    minute: Int,
-    second: Int,
-    nanoOfSecond: Int,
-    zone: ZoneId
-): ZonedDateTime {
-    return ZonedDateTime.of(year,month,dayOfMonth,hour,minute,second,nanoOfSecond,zone,)
-}
-
-// utc iso formatted string -> bool is this week of not
-// 2022-08-19T20:00Z -> true
 fun isThisWeek(isoDateTime: String): Boolean {
     val eventDateTime: ZonedDateTime = ZonedDateTime.parse(isoDateTime, DateTimeFormatter.ISO_ZONED_DATE_TIME)
     val currentDateTime: ZonedDateTime = ZonedDateTime.now(ZoneId.of("Etc/UTC"))
