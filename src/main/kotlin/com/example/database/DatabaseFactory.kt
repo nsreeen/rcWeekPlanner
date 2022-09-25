31c26b9084b10a7a23af51ce08ab3bbab0690992
Nasreen </>
2022-09-25 15:45:20 +0100

    Make db config more explicit

--- src/main/kotlin/com/example/database/DatabaseFactory.kt
+++ src/main/kotlin/com/example/database/DatabaseFactory.kt
@@ -7,8 +7,16 @@ import org.jetbrains.exposed.sql.transactions.experimental.*
 
 object DatabaseFactory {
     fun init(dbUrl: String) {
+        val dbConfig = dbUrl.splitToSequence("//").toList()[1]
+        val parts = dbConfig.splitToSequence(":", "@", "/").toList()
+        val username = parts[0]
+        val password = parts[1]
+        val host = parts[2]
+        val port = parts[3]
+        val dbname = parts[4]
+        val url = "jdbc:postgresql://$host:$port/$dbname"
         val driverClassName = "org.postgresql.Driver"
-        val database = Database.connect(dbUrl, driverClassName)
+        val database = Database.connect(url, driverClassName, user=username, password=password)
         transaction(database) {
             SchemaUtils.create(EventRows)
             SchemaUtils.create(CalendarRows)
